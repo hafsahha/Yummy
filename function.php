d8c028803504af894d13d29f208e8fe226a37889
haf <152606727+hafsahha@users.noreply.github.com>
2024-05-31 15:54:56 +0700

    update

--- function.php
+++ function.php
@@ -32,3 +32,122 @@
         
         return $result;
     }
+
+    function addPenyewa($data){
+        global $conn;
+    
+        $nama = $data['nama'];
+        $noHP = $data['no_hp'];
+        $member = $data['membership'];
+    
+        $query = "CALL add_penyewa('$nama', '$noHP', $member)";
+        $result = mysqli_query($conn, $query);
+    
+        $isSucceed = mysqli_affected_rows($conn);
+    
+        return $isSucceed;
+    }
+
+    function readPenyewa(){
+        global $conn;
+    
+        $query = "SELECT * FROM penyewa";
+        $result = mysqli_query($conn, $query);
+    
+        return $result;
+    }    
+
+    function addLapangan($data){
+        global $conn;
+    
+        $jenisLapangan = $data['jenis_lapangan'];
+        $jenisOlahraga = $data['jenis_olahraga'];
+        $fasilitas = $data['fasilitas_umum'];
+        $harga = $data['harga'];
+        $pj = $data['ID_PJ'];
+    
+        $query = "CALL add_lapangan('$jenisLapangan', '$jenisOlahraga', '$fasilitas', $harga, $pj)";
+        $result = mysqli_query($conn, $query);
+    
+        $isSucceed = mysqli_affected_rows($conn);
+    
+        return $isSucceed;
+    }
+
+    function readLapangan(){
+        global $conn;
+    
+        $query = "SELECT * FROM lapangan";
+        $result = mysqli_query($conn, $query);
+    
+        return $result;
+    }
+    
+    function addFasilitas($data){
+        global $conn;
+    
+        $nama = $data['nama_fasilitas'];
+        $harga = $data['harga'];
+    
+        $query = "CALL add_fasilitas('$nama', $harga)";
+        $result = mysqli_query($conn, $query);
+    
+        $isSucceed = mysqli_affected_rows($conn);
+    
+        return $isSucceed;
+    }
+
+    function readFasilitas(){
+        global $conn;
+    
+        $query = "SELECT * FROM fasilitas";
+        $result = mysqli_query($conn, $query);
+    
+        return $result;
+    }
+
+    function addPJ($data){
+        global $conn;
+    
+        $nama = $data['nama'];
+        $noHP = $data['no_hp'];
+    
+        $query = "CALL add_pj('$nama', '$noHP')";
+        $result = mysqli_query($conn, $query);
+    
+        $isSucceed = mysqli_affected_rows($conn);
+    
+        return $isSucceed;
+    }
+
+    function readPJ(){
+        global $conn;
+    
+        $query = "SELECT * FROM pj";
+        $result = mysqli_query($conn, $query);
+    
+        return $result;
+    }
+
+    function addTransaksiFasilitas($data){
+        global $conn;
+    
+        $IDFasilitas = $data['ID_fasilitas'];
+        $IDTransaksi = $data['ID_transaksi'];
+    
+        $query = "CALL add_transaksi_fasilitas($IDFasilitas, $IDTransaksi)";
+        $result = mysqli_query($conn, $query);
+    
+        $isSucceed = mysqli_affected_rows($conn);
+    
+        return $isSucceed;
+    }
+
+    function readTransaksiFasilitas(){
+        global $conn;
+    
+        $query = "SELECT * FROM transaksi_fasilitas";
+        $result = mysqli_query($conn, $query);
+    
+        return $result;
+    }
