2ce67bd6d191e1f8b3fe47f98d18d40f5ccfdff8
Daneswara <150428419+codewara@users.noreply.github.com>
2024-06-01 11:41:53 +0700

    Update getLapangan.php

--- getLapangan.php
+++ getLapangan.php
@@ -2,21 +2,21 @@
     include("function.php");
 
     global $conn;
-    $query = "SELECT * FROM lapangan";
+    $id = $_GET['id'];
+    $query = "SELECT * FROM lapangan WHERE ID = $id";
 
     $result = mysqli_query($conn, $query);
 
     if($result) {
-        while($temp = mysqli_fetch_assoc($result)) {
-            $datas[] = [
-                "ID" => $temp["ID"],
-                "harga" => $temp["harga"],
-                "gambar" => $temp["gambar"],
-            ];
-        }
+        $temp = mysqli_fetch_assoc($result)) {
+        $datas[] = [
+            "ID" => $temp["ID"],
+            "harga" => $temp["harga"],
+            "gambar" => $temp["gambar"],
+        ];
     }
 
-    mysqli_close($conn_regency);
+    mysqli_close($conn);
 
     header('Content-Type: application/json');
     echo json_encode($datas);
