cd4f45a4f9d2e6afff788cc4848871937feaef25
Daneswara <150428419+codewara@users.noreply.github.com>
2024-06-01 10:28:53 +0700

    Update ajax.js

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -12,14 +12,14 @@ $(document).ready(function () {
             $('#non_member input').prop('required', true);
         }
     });
-    
-    $('#form_image').css('background-image', 'url(assets/img/events-2.jpg)');
+    
     $.ajax({
         url: 'getLapangan.php?id=' + $('#ID_lapangan').val(),
         type: 'GET',
         datatype: 'json',
         success: function(response) {
             console.log(response);
+            $('#form_image').css('background-image: url(assets/img/lapangan/' + response.gambar);
         }
     });
     
