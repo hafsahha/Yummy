2655d12bcb8518375263b670aa6036d630696907
haf <152606727+hafsahha@users.noreply.github.com>
2024-06-12 10:10:07 +0700

    update

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
           <li><a href="#hero">Home</a></li>
           <li><a href="#gallery">Gallery</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#menu">Menu</a></li>
+          <li><a href="#menu">Fields</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav><!-- .navbar -->
