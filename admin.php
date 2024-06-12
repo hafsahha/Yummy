dfa9481ad78a4e2b6e2b9482a58876d31df78039
haf <152606727+hafsahha@users.noreply.github.com>
2024-06-12 18:03:58 +0700

    ganti button reservasi jadi admin

--- admin.php
+++ admin.php
@@ -63,7 +63,7 @@ if (isset($_GET['id'])) {
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>[Template] Sample Inner Page</title>
+  <title>Admin Page</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -115,7 +115,7 @@ if (isset($_GET['id'])) {
                 </ul>
             </nav><!-- .navbar -->
 
-            <a class="btn-book-a-table" href="transaksi.php">Reservasi Lapangan</a>
+            <a class="btn-book-a-table" href="admin.php">Admin Page</a>
 
         </div>
     </header><!-- End Header -->
